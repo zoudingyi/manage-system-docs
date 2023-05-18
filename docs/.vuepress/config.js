5a243fb66d4d8965c6aaea7826727a2d597fbda3
zoudingyi <zoudingyi@qq.com>
2023-05-18 17:21:51 +0800

    :rocket: first release(init docs.):

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: '/',
+  title: 'Vue3 System Template',
+  description: '基于RuoYi系统二次开发的管理系统模板开发文档',
+  themeConfig: {
+    logo: '/assets/img/favicon.ico',
+    nav: [
+      { text: '文档', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/zoudingyi/manage-system-docs' },
+    ],
+    sidebar: "auto",
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: /assets/img/hero.png
+heroText: Vue3 System Template Documents
+tagline: 基于RuoYi系统二次开发的管理系统模板开发文档
+actionText: 快速上手 →
+actionLink: /guide/
+features: false
+footer: false
+---
